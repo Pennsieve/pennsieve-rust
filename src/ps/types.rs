4bcf15071276276f8399570ccc7bdd6117f10f83
Li Dinghong <networkz2019@gmail.com>
2021-12-10 11:31:24 -0500

    update

--- src/ps/types.rs
+++ src/ps/types.rs
@@ -8,12 +8,21 @@ use crate::ps::error;
 
 pub use crate::ps::error::{Error, ErrorKind, Result};
 
+use futures::TryFutureExt;
+
 // 0.1 -> 0.3: 
 // change all occurrences of `Future<Item=Foo, Error=Bar>` to `Future<Output=Result<Foo, Bar>>` 
 
 /// A `futures::future::Future` type parameterized by `ps::error::Error`
+trait NewTrait {}
+
 #[allow(dead_code)]
-pub type Future<T> = Box<dyn futures::Future<Output = Result<T>> + Send>;
+pub type Future<T> = <Box<(dyn NewTrait + 'static)> as Box>::Pin;
+
+impl<T> NewTrait for Future<T>
+where
+	Future<T>: futures::Future<Output = Result<T>> + TryFutureExt + Send 
+{}
 
 /// A `futures::stream::Stream` type parameterized by `ps::error::Error`
 #[allow(dead_code)]
